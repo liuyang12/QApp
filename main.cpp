f8410adf2c8e5a32801aba4143e5929fda790913
Liu Yang <liuyang12@mails.tsinghua.edu.cn>
2014-12-07 15:13:24 +0800

    Ver. 0.1.1 demo of git Hello World!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "qwindow.h"
 #include <QApplication>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWindow w;
     w.show();
+
+    printf("Hello World!\n");
 
     return a.exec();
 }
